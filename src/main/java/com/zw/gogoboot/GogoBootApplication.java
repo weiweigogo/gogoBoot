b8b90d872f7a422c148cda256b1976154e0c8b36
zw <15733179606@163.com>
2025-03-15 23:04:46 +0800

    初始换项目

---
+++ src/main/java/com/zw/gogoboot/GogoBootApplication.java
@@ -0,0 +1,13 @@
+package com.zw.gogoboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GogoBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GogoBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zw/gogoboot/GogoBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zw.gogoboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GogoBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
